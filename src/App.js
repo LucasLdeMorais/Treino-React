33b37cb9cfd9e11b6bec1d7b16fd86ad762b3cb9
Lucas L. Morais <lucas.lopes@uniriotec.br>
2018-12-07 13:22:50 -0200

    Trabalhando funções em arquivos distintos

--- src/App.js
+++ src/App.js
@@ -3,40 +3,68 @@ import React, { Component } from 'react';
 //components
 import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
+
 //Stylesheets
 import './Assets/css/default.min.css';
+import './Assets/css/header.min.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      input : ''
+    }
+
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+
+  updateInput(event){
+    this.setState({input : event.target.value})
+  }
+
+  handleSubmit(){
+    console.log('Your input value is: ' + this.state.input)
+    //Send state to the server code
+  }
+
   render() {
     /*JavaScript here*/
     var x = 2;
     var y = 2;
-
-    var z = addTwoNumbers(x,y);
-
+    var z = this.state.input;
     function addTwoNumbers(x, y) {
-       return x + y;
+      return x + y;
     }
 
     function checkZ(z,x,y){
       if (z == x+y) {
         return "acertô, miserávi";
+      } else if (z == '') {
       } else {
-        return "Errrrrou";
+          return "Errrrrou. Era pra ser " + addTwoNumbers(x, y);
       }
     }
 
 
-  return (
-    /* HTML here */
-    <div className="App">
-      <Header />
-      <div id="teste">
-        {x}+{y}={z} <br></br>
-        {checkZ(z,x,y)}
+    return (
+      /* HTML here */
+      <div className="App">
+        <Header />
+          <main>
+            <div id="teste">
+              <h2>Digite a resposta correta</h2>
+              <form id="frm1" action="/App.js">
+              {x} + {y} = <input type="text" onChange={this.updateInput}></input><br></br>
+              </form>
+              <div id="resposta">
+                {checkZ(z,x,y)}
+              </div>
+            </div>
+          </main>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     );
   }
 }
--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -3,18 +3,44 @@ import React, { Component } from 'react';
 //stylesheets
 
 class Header extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      num : 1
+    }
+
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+
+  updateInput(event){
+    this.setState({num : event.target.value})
+  }
+
   render() {
+    var num = this.state.num;
+
+    function addOne(num){
+      return  num + 1;
+    }
+
     return (
       <header>
 
-      <div className="logo">
+      <h1 className="logo">
         Header
-      </div>
+      </h1>
 
       <nav>
-
         <ul>
-
+          <li>link {num}</li>
+          <li>link {num}</li>
+          <li>link {num}</li>
+          <li>link {num}</li>
+          <li>link {num}</li>
+          <li>link {num}</li>
+          <li><button onClick={this.updateInput}>clique aqui para +1</button></li>
         </ul>
 
       </nav>
