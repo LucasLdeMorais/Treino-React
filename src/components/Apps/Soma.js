f16f270dc225a9e86a0a46251da88ca5ec05c7c4
Lucas L. Morais <lucas.lopes@uniriotec.br>
2019-01-07 13:25:13 -0200

    Notification

--- src/components/Apps/Soma.js
+++ src/components/Apps/Soma.js
@@ -34,13 +34,46 @@ class Soma extends Component {
 
     function checkZ(z,x,y){
       if (z == x+y) {
+        notifyMe()
         return "acertô, miserávi";
       } else if (z == '') {
       } else {
           return "Errrrrou. Era pra ser " + addTwoNumbers(x, y);
       }
     }
-    
+
+    function notifyMe() {
+      // Verifica se o browser suporta notificações
+      if (!("Notification" in window)) {
+        alert("Este browser não suporta notificações de Desktop");
+      }
+
+      // Let's check whether notification permissions have already been granted
+      else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+      }
+
+      // Otherwise, we need to ask the user for permission
+      else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            var notification = new Notification("Hi there!");
+          }
+        });
+      }
+
+      // At last, if the user has denied notifications, and you
+      // want to be respectful there is no need to bother them any more.
+      }Notification.requestPermission();function spawnNotification(corpo,icone,titulo) {
+      var opcoes = {
+          body: corpo,
+          icon: icone
+      }
+      var n = new Notification(titulo,opcoes);
+  }
+
     return (
       /* HTML here */
       <main>
